350b940d011be33f85239c6366e170bd3cd823bf
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-22 13:34:04 -0500

    Upsolving Codeforces Round 665

--- Contests/Codeforces Round 665/A.cpp
+++ Contests/Codeforces Round 665/A.cpp
@@ -6,72 +6,16 @@ using namespace std;
 void solve() {
     int n, k;
     cin >> n >> k;
-    if (k == 0) {
-        cout << (n % 2 == 0 ? 0 : 1) << '\n';
-    } else if (n < k) {
-        cout << (k - n) << '\n'; 
-    } else if (n > k) {
+    if (n < k) {
+        cout << (k - n) << '\n';
+    } else if (n % 2 == k % 2) {
         cout << 0 << '\n';
     } else {
-        cout << 0 << '\n';
-    }
-}
-
-int option1(int n, int k) {
-    if (k == 0) {
-        return (n % 2 == 0 ? 0 : 1);
-    } else if (n < k) {
-        return (k - n);
-    } else if (n > k) {
-        return 0;
-        //return (n % 2 == 0 && k % 2 == 0 ? 0 : 1);
-    } else {
-        return 0;
-    }
-}
-
-int option2(int n, int k) {
-    int res = 0;
-    if (k == 0) {
-        return (n % 2 == 0 ? 0 : 1);
-    }
-    while (n <= (int) 1e6) {
-        for (int i = 0; i <= n; i++) {
-            if (n - i == k)
-                return res;
-        }
-        n++;
-        res++;
-    }
-    return res;
-}
-
-void solve2() {
-    int n, k;
-    cin >> n >> k;
-    cout << option2(n, k) << '\n';
-}
-
-void stress_test() {
-    while (true) {
-        int n = rand() % 100 + 10;
-        int k = rand() % 100 + 10;
-        int res1 = option1(n, k);
-        int res2 = option2(n, k);
-        cout << "n: " << n << ", k: " << k << '\n'; 
-        if (res1 != res2) {
-            cout << "Wrong answer\n";
-            cout << "Correct answer: " << res2 << '\n';
-            cout << "User's answer:  " << res1 << '\n';
-            break;
-        } else {
-            cout << "OK\n";
-        }
+        cout << 1 << '\n';
     }
 }
 
 int main() {
-    stress_test();
     int t = 0;
     cin >> t;
     for (int i = 0; i < t; i++)
--- Contests/Codeforces Round 665/C.cpp
+++ Contests/Codeforces Round 665/C.cpp
@@ -2,38 +2,22 @@
 
 using namespace std;
 
-int gcd(int a, int b) {
-    if (b > a)
-        swap(a, b);
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
-
 void solve() {
     int n = 0;
     cin >> n;
     vector<int> numbers(n);
+    vector<int> numbers2(n);
     int m = INT_MAX;
     for (int i = 0; i < n; i++) {
         cin >> numbers[i];
         m = min(m, numbers[i]);
+        numbers2[i] = numbers[i];
     }
+    sort(numbers2.begin(), numbers2.end());
     for (int i = 0; i < n; i++) {
-        if (numbers[i] > numbers[i + 1 % n]) {
-            bool found = false;
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    if (gcd(numbers[i], numbers[j]) == m) {
-                        found = true;
-                        break;
-                    }
-                }
-            }
-            if (!found) {
-                cout << "NO\n";
-                return;
-            }
+        if (numbers[i] != numbers2[i] && numbers[i] % m != 0) {
+            cout << "NO\n";
+            return;
         }
     }
     cout << "YES\n";
