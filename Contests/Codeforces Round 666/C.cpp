efb6762d8caa001591dc0203e0328b137800d763
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-09-01 20:59:56 -0500

    Solving C problem round 666

--- Contests/Codeforces Round 666/C.cpp
+++ Contests/Codeforces Round 666/C.cpp
@@ -5,28 +5,32 @@ using namespace std;
 int main() {
     int n = 0;
     cin >> n;
-    long long maximum = LONG_LONG_MIN;
     vector<long long> numbers(n);
     for (int i = 0; i < n; i++) {
         cin >> numbers[i];
-        maximum = max(maximum, numbers[i]);
     }
-    long long div = maximum / n;
-    div = div * n + 1;
-    cout << 1 << ' ' << n << '\n';
-    for (int i = 0; i < n; i++) {
-        cout << abs(numbers[i] - div) << ' ';
+    if (n == 1) {
+        for (int i = 0; i < 3; i++) {
+            cout << 1 << ' ' << 1 << '\n';
+            if (i == 2) {
+                cout << -numbers[0] << '\n';
+            } else {
+                cout << 0 << '\n';
+            }
+        }
+        return 0;
     }
-    cout << '\n';
-    cout << 1 << ' ' << n << '\n';
-    for (int i = 0; i < n; i++) {
-        cout << div * -1 << ' ';
-    }
-    cout << '\n';
-    cout << 1 << ' ' << n << '\n';
-    for (int i = 0; i < n; i++) {
-        cout << 0 << ' ';
+    cout << 1 << ' ' << (n - 1) << '\n';
+    for (int i = 0; i < n - 1; i++) {
+        cout << numbers[i] * (n - 1) << ' ';
     }
     cout << '\n';
+    cout << 1 << ' ' << n << '\n';
+    for (int i = 0; i < n; i++) {
+        cout << -numbers[i] * n << ' ';
+    }
+    cout << '\n';
+    cout << n << ' ' << n << '\n';
+    cout << numbers[n - 1] * (n - 1) << '\n';
     return 0;
 }
