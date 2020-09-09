a9e256e62e1a8ceaf972be3b334ff389e667016b
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-09-09 09:04:58 -0500

    Upsolving Codeforces Round 669

--- Contests/Codeforces Round 669/A.cpp
+++ Contests/Codeforces Round 669/A.cpp
@@ -5,72 +5,23 @@ using namespace std;
 
 void solve() {
     int n = 0; cin >> n;
-    vector<int> elements(n);
-    bool check = true;
-    int k = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> elements[i];
-        if (k % 2 != 0 && check) {
-            if (elements[i - 1] != elements[i]) {
-                check = false;
-            }
-        }
-        k++;
-    }
-    if (check) {
-        cout << n << '\n';
-        for (int i = 0; i < n; i++) {
-            cout << elements[i] << ' ';
-        }
-        cout << '\n';
-    } else {
-        if (n == 2) {
-            cout << 1 << '\n';
-            cout << 0 << '\n';
-            return;
-        }
-        int index1 = 0;
-        int index2 = 1;
-        int s = 0;
-        cout << (n / 2) << '\n';
-        while (s < n / 2) {
-            if (elements[index1] == elements[index2]) {
-                cout << elements[index1] << ' ' << elements[index2] << ' ';
-                s += 2;
-                index1++;
-                index2 = index1 + 1;
-            } else {
-                index2++;
-                if (index2 == n - 1) {
-                    index1++;
-                    index2 = index1 + 1;
-                }
-            }
-        }
-        cout << '\n';
-    }
-}
-
-void solve2() {
-    int n = 0; cin >> n;
-    vector<int> count(2);
+    int cnt_zeros = 0, cnt_ones = 0;
     int element;
     for (int i = 0; i < n; i++) {
         cin >> element;
-        count[element]++;
+        if (element == 0) cnt_zeros++;
+        if (element == 1) cnt_ones++;
     }
-
-    if (count[0] >= n / 2) {
+    if (cnt_zeros >= n / 2) {
         cout << n / 2 << '\n';
         for (int i = 0; i < n / 2; i++) {
             cout << 0 << ' ';
         }
         cout << '\n';
     } else {
-        int len = n / 2;
-        if (len % 2 != 0) {
+        int len = (n / 2);
+        if (len % 2 != 0)
             len++;
-        }
         cout << len << '\n';
         for (int i = 0; i < len; i++) {
             cout << 1 << ' ';
@@ -82,7 +33,7 @@ void solve2() {
 int main() {
     int t = 0; cin >> t;
     for (int i = 0; i < t; i++) {
-        solve2();
+        solve();
     }
     return 0;
 }
--- Contests/Codeforces Round 669/B.cpp
+++ Contests/Codeforces Round 669/B.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 
 void solve() {
-    
+    int n = 0; cin >> n;
+    vector<int> sequence(n);
+    for (int i = 0; i < n; i++) {
+        cin >> sequence[i];
+    }
+    sort(sequence.rbegin(), sequence.rend());
+    int m = INT_MIN;
+    int gcd = sequence[0];
+    for (int i = 1; i < n - 1; i++) {
+        int option = __gcd(gcd, sequence[i]);
+        int index = i;
+        for (int j = i + 1; j < n; j++) {
+            int option2 = __gcd(gcd, sequence[j]);
+            if (option2 > option) {
+                index = j;
+                option = option2;
+            }
+        }
+        gcd = option;
+        swap(sequence[i], sequence[index]);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << sequence[i] << ' ';
+    }
+    cout << '\n';
 }
 
 int main() {
