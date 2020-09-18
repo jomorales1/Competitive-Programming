c9288eb8f0765ff712b2829629e699a43b2b94ff
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-09-18 13:57:54 -0500

    Problem 805A solved

---
+++ Practice/805A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int left, right;
+    cin >> left >> right;
+    if (left == right) {
+        cout << left;
+    } else {
+        cout << 2 << '\n';
+    }
+    return 0;
+}
